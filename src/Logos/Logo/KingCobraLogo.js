b64983a1f209abaae1f54364cf0d410f222595ed
donovanodom <dodomdev@gmail.com>
2022-04-06 09:08:29 -0700

    Simplified styling for DRY code

--- src/Logos/Logo/KingCobraLogo.js
+++ src/Logos/Logo/KingCobraLogo.js
@@ -3,16 +3,14 @@ import KingCobra from "../svgs/KingCobra";
 const KingCobraLogo = () => {
   return (
     <div className="logos">
-      <div className="orca">
-        <div className="logo-grid">
-          <KingCobra />
-          <div className="logo-details">
-            <ul style={{ padding: "0", listStyle: "none", textAlign: "left" }}>
-              <li>Letter: K</li>
-              <li>Animal: KingCobra </li>
-              <li>Description: N/A</li>
-            </ul>
-          </div>
+      <div className="logo-grid">
+        <KingCobra />
+        <div className="logo-details">
+          <ul style={{ padding: "0", listStyle: "none", textAlign: "left" }}>
+            <li>Letter: K</li>
+            <li>Animal: KingCobra </li>
+            <li>Description: N/A</li>
+          </ul>
         </div>
       </div>
     </div>
--- src/Logos/Logo/OrcaLogo.js
+++ src/Logos/Logo/OrcaLogo.js
@@ -3,16 +3,14 @@ import Orca from "../svgs/Orca";
 const OrcaLogo = () => {
   return (
     <div className="logos">
-      <div className="orca">
-        <div className="logo-grid">
-          <Orca />
-          <div className="logo-details">
-            <ul style={{ padding: "0", listStyle: "none", textAlign: "left" }}>
-              <li>Letter: O</li>
-              <li>Animal: Orca Whale</li>
-              <li>Description: N/A</li>
-            </ul>
-          </div>
+      <div className="logo-grid">
+        <Orca />
+        <div className="logo-details">
+          <ul style={{ padding: "0", listStyle: "none", textAlign: "left" }}>
+            <li>Letter: O</li>
+            <li>Animal: Orca Whale</li>
+            <li>Description: N/A</li>
+          </ul>
         </div>
       </div>
     </div>
