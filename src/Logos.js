b4b36e0d0eb6b4aa4bf21628ba84a1c775dfb52b
donovanodom <dodomdev@gmail.com>
2022-04-06 08:18:16 -0700

    init styling list in first draft of logo component design

--- src/Logos.js
+++ src/Logos.js
@@ -10,7 +10,16 @@ const Logos = () => {
             src="https://images.unsplash.com/photo-1598202604734-f6fcd12b2384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           />
         </div>
-        <Orca />
+        <div className="logo-grid">
+          <Orca />
+          <div className="logo-details">
+            <ul style={{ padding: "0", listStyle: "none", textAlign: "left" }}>
+              <li>Letter: O</li>
+              <li>Animal: Orca Whale</li>
+              <li>Description: N/A</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
